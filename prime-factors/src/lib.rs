50e876879daa50fa7efc4c5c3f7e001eb8cf72e4
ktunprasert <ktunprasert@outlook.com>
2023-12-24 22:16:49 +0000

    prime-factors: n mutable

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,17 +1,15 @@
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors(mut n: u64) -> Vec<u64> {
     if n < 2 {
         return vec![]
     }
 
-    let mut n_copy = n;
-
     let mut prime_factors = vec![];
     let mut factor = 2;
 
-    while n_copy != 1 {
-        if n_copy % factor == 0 {
+    while n != 1 {
+        if n % factor == 0 {
             prime_factors.push(factor);
-            n_copy /= factor;
+            n /= factor;
         } else {
             factor += 1;
         }

019d2c6c78e887e9b864cd01e7d07a26b0105ee6
ktunprasert <ktunprasert@outlook.com>
2023-12-24 21:31:46 +0000

    leap

---
+++ leap/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn is_leap_year(year: u64) -> bool {
+    match year {
+        year if year % 400 == 0 => true,
+        year if year % 100 == 0 => false,
+        _ => year % 4 == 0,
+    }
+}

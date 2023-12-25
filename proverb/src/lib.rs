689b451e5c957adfd3e43e3ac8d073207ee484bc
ktunprasert <ktunprasert@outlook.com>
2023-12-25 16:35:04 +0000

    proverb: functional

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,22 +1,12 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
-
-    let mut proverb = String::new();
-
-    for i in 0..list.len() - 1 {
-        proverb.push_str(&format!(
-            "For want of a {} the {} was lost.\n",
-            list[i],
-            list[i + 1]
-        ));
-    }
-
-    proverb.push_str(&format!(
-        "And all for the want of a {}.",
-        list.first().unwrap_or(&"")
-    ));
-
-    proverb
 }

62d031bda3074c253c7bbee860dcc9d905e45a6b
ktunprasert <ktunprasert@outlook.com>
2023-12-25 23:58:48 +0000

    sum-of-multiples: one pass

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,16 +1,5 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    factors
-        .iter()
-        .filter(|&&x| x != 0)
-        .fold(HashSet::new(), |mut acc: HashSet<u32>, &x| {
-            (x..limit).step_by(x as usize).into_iter().for_each(|x| {
-                acc.insert(x);
-            });
-
-            acc
-        })
-        .iter()
+    (1..limit)
+        .filter(|x| factors.iter().any(|y| y > &0 && x % y == 0))
         .sum()
 }

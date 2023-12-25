ddc11b1434df78f796dfd5933544b38cf61f2086
ktunprasert <ktunprasert@outlook.com>
2023-12-25 16:45:47 +0000

    raindrops: with_capacity

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn raindrops(n: u32) -> String {
     let map = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
-    let mut output = String::new();
+    let mut output = String::with_capacity(15);
     for (factor, sound) in map.iter() {
         if n % factor == 0 {
             output.push_str(sound);

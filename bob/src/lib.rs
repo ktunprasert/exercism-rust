11ea67cbea5e965ae9fe84fa7b5ea8be988c84da
ktunprasert <ktunprasert@outlook.com>
2023-12-26 00:21:34 +0000

    bob: non-obnoxious match statement

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -2,13 +2,12 @@ pub fn reply(message: &str) -> &str {
     let alphas = message.chars().filter(|c| c.is_alphabetic()).collect::<Vec<char>>();
     let is_question = message.trim().ends_with('?');
     let yelling = !alphas.is_empty() && alphas.iter().all(|c| c.is_uppercase());
-    let silence = message.trim().is_empty();
 
-    match (is_question, yelling, silence) {
-        (true, true, _) => "Calm down, I know what I'm doing!",
-        (true, _, _) => "Sure.",
-        (_, true, _) => "Whoa, chill out!",
-        (_, _, true) => "Fine. Be that way!",
+    match message.trim() {
+        _m if is_question && yelling => "Calm down, I know what I'm doing!",
+        _m if is_question => "Sure.",
+        _m if yelling => "Whoa, chill out!",
+        "" => "Fine. Be that way!",
         _ => "Whatever.",
     }
 }

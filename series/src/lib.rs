63305eceb98f1bb24d8272a055c269054a143228
ktunprasert <ktunprasert@outlook.com>
2025-05-11 21:36:28 +0100

    rust: series

---
+++ series/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|f| f.iter().collect::<String>())
+        .collect()
+}

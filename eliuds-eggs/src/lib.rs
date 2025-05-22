b0ffef4acaa06d3296cd91a84b45ab4580a8adb6
ktunprasert <ktunprasert@outlook.com>
2025-05-22 21:49:09 +0100

    rust.eggs: one arm

--- eliuds-eggs/src/lib.rs
+++ eliuds-eggs/src/lib.rs
@@ -1,7 +1,6 @@
 pub fn egg_count(dv: u32) -> usize {
     match dv {
         0 => 0,
-        _ if dv & 1 == 1 => 1 + egg_count(dv >> 1),
-        _ => egg_count(dv >> 1),
+        _ => (dv & 1) as usize + egg_count(dv >> 1),
     }
 }

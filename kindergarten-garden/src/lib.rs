5e6cf431d392c63d7dcea1afcc94c64022c71e1b
ktunprasert <ktunprasert@outlook.com>
2025-05-22 20:28:09 +0100

    rust.kinder: ok

--- kindergarten-garden/src/lib.rs
+++ kindergarten-garden/src/lib.rs
@@ -1,20 +1,19 @@
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let student_idx: usize = (student.as_bytes()[0] - 'A' as u8).into();
+    let student_idx: usize = (student.as_bytes()[0] - b'A') as usize;
 
-    let plants: Vec<char> = diagram
+    diagram
         .lines()
         .flat_map(|line| line.chars().skip(student_idx * 2).take(2))
-        .collect();
-
-    println!("plants: {:?}", plants);
-
-    plants.iter().map(|c| -> &'static str {
-        match c {
-            'V' => "violets",
-            'R' => "radishes",
-            'C' => "clover",
-            'G' => "grass",
-            _ => panic!("Unknown plant"),
-        }
-    }).collect()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|c| -> &'static str {
+            match c {
+                'V' => "violets",
+                'R' => "radishes",
+                'C' => "clover",
+                'G' => "grass",
+                _ => panic!("Unknown plant"),
+            }
+        })
+        .collect()
 }

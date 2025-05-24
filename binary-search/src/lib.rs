8f01f748247fa769044565852ffed84bc037c586
ktunprasert <ktunprasert@outlook.com>
2025-05-24 03:41:14 +0100

    rust.binsearch: OK

---
+++ binary-search/src/lib.rs
@@ -0,0 +1,30 @@
+use std::cmp::Ordering;
+
+fn search(array: &[i32], key: i32, idx: usize) -> Option<usize> {
+    if array.len() == 1 && key != array[0] {
+        return None;
+    }
+
+    println!("{}", key);
+
+    let n = array.len() / 2;
+    println!("a: {:?}, n: {}, idx: {}", array, n, idx);
+
+    let cond = key.cmp(&array[n]);
+
+    println!("{:?}", cond);
+
+    match cond {
+        Ordering::Equal => Some(n + idx),
+        Ordering::Less => search(&array[..n], key, idx.min(n)),
+        Ordering::Greater => search(&array[n..], key, idx + n),
+    }
+}
+
+pub fn find(array: &[i32], key: i32) -> Option<usize> {
+    if array.is_empty() || array.len() == 0 {
+        return None;
+    }
+
+    search(array, key, 0)
+}

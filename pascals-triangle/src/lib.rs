a533e15160431c3b1cbf0b9125b19240ac55cd5a
ktunprasert <ktunprasert@outlook.com>
2025-05-24 17:40:58 +0100

    rust.pascals-tri: washing into chain dot

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -13,25 +13,22 @@ impl PascalsTriangle {
         let mut out: Vec<Vec<u32>> = Vec::with_capacity(self.count);
         let mut prev = vec![];
 
-        if self.count == 0 {
-            return out;
-        }
-
         if self.count >= 1 {
             out.push(vec![1]);
             prev = vec![1];
         }
 
-        for _ in 1..self.count {
-            let current: Vec<u32> = once(1)
-                .chain(prev.windows(2).map(|pair| pair[0] + pair[1]))
-                .chain(once(1))
-                .collect();
+        (1..self.count)
+            .fold((out, prev), |(mut acc, prev), _| {
+                let current: Vec<u32> = once(1)
+                    .chain(prev.windows(2).map(|pair| pair[0] + pair[1]))
+                    .chain(once(1))
+                    .collect();
 
-            out.push(current.clone());
-            prev = current.clone();
-        }
+                acc.push(current.clone());
 
-        out
+                (acc, current)
+            })
+            .0
     }
 }

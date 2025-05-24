3d6c4d64d2907c567c9398901f4608617a13d672
ktunprasert <ktunprasert@outlook.com>
2025-05-24 16:04:59 +0100

    rust.pangram: cook with two arms

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,15 +1,20 @@
+const PANGRAM: u32 = (1 << 26) - 1;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
     sentence
-        .to_ascii_lowercase()
         .chars()
-        .fold([false; 26], |mut acc, c| match c {
-            'a'..='z' => {
-                acc[(c as u8 - b'a') as usize] = true;
-                acc
+        .try_fold(0, |mut seen, c| match c {
+            _ if seen == PANGRAM => Err(()),
+            'A'..='Z' => {
+                seen |= 1 << (c as u8 - b'A');
+                Ok(seen)
             }
-            _ => acc,
+            'a'..='z' => {
+                seen |= 1 << (c as u8 - b'a');
+                Ok(seen)
+            }
+            _ => Ok(seen),
         })
-        .iter()
-        .all(|&b| b)
+        .map_or(true, |seen| seen == PANGRAM)
 }
